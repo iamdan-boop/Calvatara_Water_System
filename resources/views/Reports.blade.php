ae1ac4672701987fe1c394663a85d16a7d254d31
lieking029 <89558128+lieking029@users.noreply.github.com>
2022-02-01 15:48:02 +0800

    design in reports
    reports design

--- resources/views/Reports.blade.php
+++ resources/views/Reports.blade.php
@@ -2,43 +2,41 @@
 
 @section('dashboard-content')
     
-   <section class="ml-5 mt-5">
-    <nav class="nav-item color">
-        <input type="text" class="bg-light  small searchi" placeholder="Filter Search"
-                    aria-label="Search" aria-describedby="basic-addon2">
-    </nav>
-    <table>
+   <section class=" mt-5">
+    <input type="text" class="form-control bg-light border-0 small " placeholder="Search for..."
+    aria-label="Search" aria-describedby="basic-addon2">    
+    <table class="table">
         <thead>
             <tr>
-                <th scope="col" class="super p-1">Name</th>
-                <th scope="col" class="super p-1">Route</th>
-                <th scope="col" class="super p-1">Type</th>
-                <th scope="col" class="super p-1">Meter Number</th>
-                <th scope="col" class="super p-1">Stub-out Number</th>
-                <th scope="col" class="super p-1">Consumption(cu.m)</th>
-                <th scope="col" class="super p-1">Bill</th>
-                <th scope="col" class="super p-1">Amount Paid</th>
-                <th scope="col" class="super p-1">Date #being Paid</th>
-                <th scope="col" class="super p-1">OR number #input</th>
-                <th scope="col" class="super p-1">Status #paid/unpaid</th>
+                <th scope="col">Name</th>
+                <th scope="col">Route</th>
+                <th scope="col">Type</th>
+                <th scope="col">Meter Number</th>
+                <th scope="col">Stub-out Number</th>
+                <th scope="col">Consumption(cu.m)</th>
+                <th scope="col">Bill</th>
+                <th scope="col">Amount Paid</th>
+                <th scope="col">Date #being Paid</th>
+                <th scope="col">OR number #input</th>
+                <th scope="col">Status #paid/unpaid</th>
             </tr>
         </thead>
-        <tbody class="super">
+        <tbody>
             <tr>
                 <th class="p-1">John Michael M. Buan</th>
                 <th class="p-1">Calamba</th>
                 <th class="p-1">Active</th>
-                <th class="p-1">43</th>
+                <th class="p-1" class="ml-2">43</th>
                 <th class="p-1">10029</th>
                 <th class="p-1">300.00</th>
                 <th class="p-1">4,000.00</th>
                 <th class="p-1">3,500.00</th>
                 <th class="p-1">01-31-2022</th>
                 <th class="p-1">02220</th>
-                <th class="super p-1">unpaid</th>
+                <th class="p-1">unpaid</th>
             </tr>
         </tbody>
-        <tbody class="super">
+        <tbody>
             <tr>
                 <th class="p-1"></th>
                 <th class="p-1"></th>
@@ -50,7 +48,7 @@
                 <th class="p-1">Total</th>
                 <th class="p-1"></th>
                 <th class="p-1"></th>
-                <th class="super p-1">Total Number of Paid</th>
+                <th class="p-1">Total Number of Paid</th>
             </tr>
         </tbody>
    </section>
